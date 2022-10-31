bc1100b081157363758df2cdbacb17f1b2e5ce5f
ChamodKalupahana <75031974+ChamodKalupahana@users.noreply.github.com>
2022-10-31 13:46:36 +0000

    tested time function

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+start_time = time.time()
+
+print('Hello World')
+
+end_time = time.time()
+time_taken = end_time - start_time
+print('Time taken =', str(time_taken))
